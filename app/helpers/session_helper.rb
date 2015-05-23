91144c9e027ea0c38061aed26e0e653ab6f515ce
8ENs <ben@coderush.ca>
2015-05-22 23:58:11 -0700

    New helpers (only using one)

---
+++ app/helpers/session_helper.rb
@@ -0,0 +1,22 @@
+# Helper methods defined here can be accessed in any controller or view in the application
+
+module WordOnSt
+  class App
+    module SessionHelper
+      # def simple_helper_method
+      # ...
+      # end
+
+      def user_logged_in?
+        session[:id]
+      end
+
+      def get_current_user
+        User.find(session[:id]) if user_logged_in?
+        # else return nil object so we can refer to get_current_user.name instead of session[:name] (option)
+      end
+    end
+
+    helpers SessionHelper
+  end
+end

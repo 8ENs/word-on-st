ebf5daa192753e6c4d99f4b10a859bda0bcabce2
8ENs <ben@coderush.ca>
2015-05-23 00:01:55 -0700

    Key for allowing post to work

--- config/apps.rb
+++ config/apps.rb
@@ -29,7 +29,8 @@ Padrino.configure_apps do
   # enable :sessions
   set :session_secret, 'a494ee009588a9262e3caa5704abf246627378e42b0935dba71ca79587cb58d5'
   set :protection, :except => :path_traversal
-  set :protect_from_csrf, true
+  set :protect_from_csrf, false
+
 end
 
 # Mounts the core application for this project
